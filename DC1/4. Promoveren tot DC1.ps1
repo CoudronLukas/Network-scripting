fad15f46e4523b3d322a5614da9e5d180cb6df0c
CoudronLukas <lukas.coudron@student.howest.be>
2021-09-22 12:19:53 +0200

    bug fix

--- DC1/4. Promoveren tot DC1.ps1
+++ DC1/4. Promoveren tot DC1.ps1
@@ -1,4 +1,11 @@
 ﻿#
 # Promoveren tot DC1 met administrator credentials
 #
-Install-ADDSDomainController -InstallDns -Credential (Get-Credential "Mijnschool\Administrator") -DomainName "intranet.mijnschool.be"
+Install-WindowsFeature -name AD-Domain-Services -IncludeManagementTools
+Test-ADDSForestInstallation -DomainName intranet.mijnschool.be -InstallDns
+Install-ADDSForest -DomainName intranet.mijnschool.be -InstallDNS
+Get-ADDomainController
+
+
+
+#Install-ADDSDomainController -InstallDns -Credential (Get-Credential "Mijnschool\Administrator") -DomainName "intranet.mijnschool.be"
