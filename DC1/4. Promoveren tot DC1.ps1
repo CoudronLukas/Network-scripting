2939412f647c2893fa03c1582d8d335a7ce66b3e
CoudronLukas <lukas.coudron@student.howest.be>
2021-09-22 11:52:21 +0200

    files added

---
+++ DC1/4. Promoveren tot DC1.ps1
@@ -0,0 +1,4 @@
+﻿#
+# Promoveren tot DC1 met administrator credentials
+#
+Install-ADDSDomainController -InstallDns -Credential (Get-Credential "Mijnschool\Administrator") -DomainName "intranet.mijnschool.be"
---
+++ DC1/5. Reverse DNS zone.ps1
@@ -0,0 +1,10 @@
+﻿#
+# Reverse DNS zone toevoegen
+#
+Add-DnsServerPrimaryZone -NetworkID “192.168.1.0/24” -ReplicationScope “Mijnschool”
+
+
+#
+# Controleren of reverse DNS zone is toegevoegd
+#
+Get-DnsServerZone
