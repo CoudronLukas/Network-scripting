f61d638c0b2c3ef4e5bf5723563230fa5b79dbbb
CoudronLukas <lukas.coudron@student.howest.be>
2021-09-22 09:23:55 +0200

    Create 1. Remote Desktop.ps1
    Enable remote desktop

---
+++ DC1/1. Remote Desktop.ps1
@@ -0,0 +1,11 @@
+﻿#
+# Enabling remote desktop
+#
+Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server' -name "fDenyTSConnections" -value 0
+Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
+
+#
+# Disable remote desktop
+#
+#Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server' -name "fDenyTSConnections" -value 1
+#Disable-NetFirewallRule -DisplayGroup "Remote Desktop"
