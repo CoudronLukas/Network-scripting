951feefb4ba205924e187f11a23583333b6539ec
CoudronLukas <lukas.coudron@student.howest.be>
2021-09-29 09:23:25 +0200

    3.1

--- DC1/3.1 Promote DC.ps1
+++ DC1/3.1 Promote DC.ps1
@@ -4,3 +4,6 @@
 Install-WindowsFeature AD-Domain-Services -IncludeManagementTools
 Install-ADDSDomainController -DomainName "intranet.mijnschool.be" -InstallDns:$true -Credential (Get-Credential "Mijnschool\administrator") -NoRebootCompletion:$true
 Restart-Computer
+
+Import-Module ADDSDeployment
+Install-ADDSForest -CreateDnsDelegation:$false -DatabasePath "C:\Windows\NTDS" -DomainMode "Win2012R2" -DomainName "intranet.mijnschool.be" -DomainNetbiosName "INTERNAL" -ForestMode "Win2012R2" -InstallDns:$true -LogPath "C:\Windows\NTDS" -NoRebootOnCompletion:$false -SysvolPath "C:\Windows\SYSVOL" -Force:$true
